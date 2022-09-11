b500bc6e53a80f7daad2a8dfab00a928e30855f3
BaekSeungHa <101560680+BaekSeungHa@users.noreply.github.com>
2022-09-11 18:24:18 +0900

    setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCF3t2pW0rcdsoITeA4BhEYQEH9aYhj9RU",
+  authDomain: "nwitter-5182a.firebaseapp.com",
+  projectId: "nwitter-5182a",
+  storageBucket: "nwitter-5182a.appspot.com",
+  messagingSenderId: "921962654311",
+  appId: "1:921962654311:web:e4f6960ceee6aff7bd82c2",
+};
+const app = initializeApp(firebaseConfig);
+export default app;
